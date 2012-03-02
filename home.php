f301978cc4e5044838f821efb50e479c55a70f48
Kamil Kiedrowski <kamego@gmail.com>
2012-03-02 10:22:56 -0600

    tidy up directories

--- home.php
+++ home.php
@@ -67,24 +67,6 @@
                             <p><?php _e("Posted by", 'organicthemes'); ?> <?php the_author_posts_link(); ?> &middot; <a href="<?php the_permalink(); ?>#respond"><?php comments_number(__("Leave a Comment", 'organicthemes'), __("1 Comment", 'organicthemes'), __("% Comments", 'organicthemes')); ?></a></p>
                         </div>
                         <div class="excerpt"><?php the_excerpt(); ?></div>
-<?php /*
-                        <div class="social_links">
-                            <div class="tweet_btn">
-                                <a href="http://twitter.com/share" class="twitter-share-button"
-                                data-url="<?php the_permalink(); ?>"
-                                data-via="<?php echo of_get_option('social_twitter_url'); ?>"
-                                data-text="<?php the_title(); ?>"
-                                data-related=""
-                                data-count="horizontal"><?php _e("Tweet", 'organicthemes'); ?></a>
-                            </div>
-                            <div class="like_btn">
-                                <fb:like href="<?php echo urlencode(get_permalink($post->ID)); ?>" layout="button_count" show_faces="false" width="100" font=""></fb:like>
-                            </div>
-                            <div class="plus_btn">
-                            	<g:plusone size="medium"></g:plusone>
-                            </div>
-                        </div>
-*/ ?>
                         <a class="read_more" href="<?php the_permalink() ?>" rel="bookmark"><?php _e("Read More", 'organicthemes'); ?></a>
                     </div>
                 </div>
